8d2fcf31d40949bc6652d6695511b373cdf8eca9
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-02-02 09:30:34 +0100

    fini

--- index.php
+++ index.php
@@ -2,3 +2,17 @@
 
 // TODO n'oubliez pas de créer votre classe dans le dossier classes.
 
+require "./classes/StrUtils.php";
+
+$cat = new StrUtils("chat");
+
+echo $cat->bold();
+echo "<br>";
+echo $cat->italic();
+echo "<br>";
+echo $cat->underline();
+echo "<br>";
+echo $cat->capitalize();
+echo "<br>";
+echo $cat->uglify();
+echo "<br>";
